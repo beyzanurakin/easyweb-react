522ecf8b04f50572b51cda47eb67e8714969f6e1
Beyzanur Akın <akinbeyzanur@gmail.com>
2023-03-01 17:58:01 +0300

    Add new packages

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import { ProSidebarProvider } from 'react-pro-sidebar'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ProSidebarProvider } from 'react-pro-sidebar';
 
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ProSidebarProvider>
-        <App />
-      </ProSidebarProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-)
+  <BrowserRouter>
+    <ProSidebarProvider>
+      <App />
+    </ProSidebarProvider>
+  </BrowserRouter>
+);

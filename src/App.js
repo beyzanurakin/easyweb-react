9820e3f6bd36b0e46ad2f6ae866918828b2561ed
Beyzanur Akın <akinbeyzanur@gmail.com>
2023-03-01 17:59:53 +0300

    Add new Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
-import ProSidebar from './pages/global/ProSidebar'
-import Navbar from './pages/global/Navbar'
-import Dashboard from './pages/dashboard/Dashboard'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import ProSidebar from './pages/global/ProSidebar';
+import Navbar from './pages/global/Navbar';
+import Dashboard from './pages/dashboard/Dashboard';
+import Overview from './components/overview/Overview';
+import IssueReport from './components/issueReport/IssueReport';
+import All from './components/issueReport/All';
+import Controls from './components/issueReport/Controls';
+import Errors from './components/issueReport/Errors';
+import Notifications from './components/issueReport/Notifications';
+import Warnings from './components/issueReport/Warnings';
+import ScannedPages from './components/scannedPages/ScannedPages';
+import ScanComparison from './components/scanComparison/ScanComparison';
 
 function App() {
   return (
@@ -10,11 +19,27 @@ function App() {
       <main className='content'>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Dashboard />}></Route>
+          <Route path='/' element={<Dashboard />}>
+            <Route path='/' element={<Navigate replace to='overview' />} />
+            <Route path='/overview' element={<Overview />}></Route>
+            <Route path='issue-report' element={<IssueReport />}>
+              <Route
+                path='/issue-report'
+                element={<Navigate replace to='all' />}
+              />
+              <Route path='all' element={<All />}></Route>
+              <Route path='errors' element={<Errors />}></Route>
+              <Route path='warnings' element={<Warnings />}></Route>
+              <Route path='notifys' element={<Notifications />}></Route>
+              <Route path='controls' element={<Controls />}></Route>
+            </Route>
+            <Route path='scanned-pages' element={<ScannedPages />}></Route>
+            <Route path='scan-comparison' element={<ScanComparison />}></Route>
+          </Route>
         </Routes>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
